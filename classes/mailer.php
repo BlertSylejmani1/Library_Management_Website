12cb1cbb3e8c667a44363d97f839022b66f45d2d
Bledarmorina18 <bledar.morina4@student.uni-pr.edu>
2026-05-26 18:28:52 +0200

    Shtesa dhe mbishkrime

--- classes/mailer.php
+++ classes/mailer.php
@@ -21,4 +21,26 @@ class Mailer
             'logged' => true,
         ];
     }
+
+    private static function log(string $to, string $subject, string $message, ?string $replyTo, bool $delivered): void
+    {
+        $directory = dirname(MAIL_LOG_FILE);
+        if (!is_dir($directory)) {
+            mkdir($directory, 0775, true);
+        }
+
+        $entry = sprintf(
+            "[%s] delivered=%s to=%s replyTo=%s subject=%s%s%s%s",
+            date('Y-m-d H:i:s'),
+            $delivered ? 'yes' : 'no',
+            $to,
+            $replyTo ?: '-',
+            $subject,
+            PHP_EOL,
+            $message,
+            PHP_EOL . str_repeat('-', 60) . PHP_EOL
+        );
+
+        file_put_contents(MAIL_LOG_FILE, $entry, FILE_APPEND);
+    }
 }
